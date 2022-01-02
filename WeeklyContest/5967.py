ff62b0ce067d0b669ec6c2eaf681f174955438ea
LianNayuan <64567296+LianNayuan@users.noreply.github.com>
2022-01-02 22:06:36 +0800

    Delete 5967.py

--- WeeklyContest/5967.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def checkString(self, s: str) -> bool:
-        for i in range(len(s)):
-            if s[i] == 'b':
-                for j in range(i+1,len(s)):
-                    if s[j] == 'a':
-                        return False
-        return True
